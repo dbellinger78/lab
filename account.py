9dced6eac02c0188ba24bc2b7146394d28953459
Dustin Bellinger <77249898+dbellinger78@users.noreply.github.com>
2022-11-17 23:58:25 -0600

    Create account.py

---
+++ account.py
@@ -0,0 +1,29 @@
+class Account:
+    def __init__(self, acccount_name):
+        self.acccount_name = acccount_name
+        self.account_balance = 0
+
+    def deposit(self, amount):
+        if amount <= 0:
+            return False
+        elif amount > 0:
+            self.account_balance = self.account_balance + amount
+            return True
+        else:
+            print("Invalid. Check deposit method code.")    
+
+    def withdraw(self, amount):
+        if amount <= 0 or amount > self.account_balance:
+            return False
+        elif amount > 0 and amount < self.account_balance:
+            self.account_balance = self.account_balance - amount
+            return True
+        else:
+            print("Invalid. Check withdraw method code.")      
+    
+    def get_balance(self): 
+        return self.account_balance
+
+    def get_name(self):
+        return self.acccount_name
+
